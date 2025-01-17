588522782be1d127e6f9bd300cb561e603b70891
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-01-17 22:50:14 +0530

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = ({dark, setDark}) => {
 
     return (
         <div className='shadow-[0_2px_10px_rgba(0,0,0,0.2)] min-h-20 w-full flex items-center'>
-            <div className='flex justify-between mx-16 items-center w-full '>
+            <div className='flex justify-between mx-4 md:mx-16 items-center w-full '>
                 <Link to={`/`}>
-                <h1 className='font-bold text-xl sm:text-3xl hover:cursor-pointer'>Where in the world?</h1></Link>
-                <div className='flex items-center text-1xl hover:cursor-pointer' onClick={handleMode}>
+                <h1 className='font-bold text-base sm:text-3xl hover:cursor-pointer'>Where in the world?</h1></Link>
+                <div className='flex items-center text-sm sm:text-xl hover:cursor-pointer' onClick={handleMode}>
                     <MdOutlineDarkMode className='m-2'/>
                     <p>{dark? 'Dark Mode': 'Light Mode'}</p>
                 </div>
